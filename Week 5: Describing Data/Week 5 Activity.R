7d07e10e51b19e750144e77a2261b2e9df58eb91
Reshi Rajan <122573204+reshi-rajan@users.noreply.github.com>
2024-09-20 09:27:34 -0500

    Week 5 Content

---
+++ Week 5: Describing Data/Week 5 Activity.R
@@ -0,0 +1,22 @@
+######## Week 5: Describing Data ##########
+
+#### Step 1: Import the Data #####
+# From the github download sample_data.RData
+
+# Remember move it into your POLS 209 folder
+
+# Then load the dataframe into the environment. 
+# HINT: you should see data pop up with '100 obs. of 4 variables'
+
+#### Step 2: Calculate Some Summary Statistics ####
+# Calculate the mean for variable x
+
+# Calculate the standard deviation (sd) for w
+
+# BONUS: Calculate the median for y
+
+#### Step 3: Check ####
+# With the person next to you see if you got the same answers (you should!)
+
+#### Step 4: Describing Distributions ####
+# This one is a little harder so I want us to do it together. 
